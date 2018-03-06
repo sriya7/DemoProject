0cfc04a78b0eefd1dbd7ef8ed0f3d0380f5090de
acer <acer@TMP243>
2018-03-06 11:31:29 +0530

    first commit

---
+++ src/com/tyss/Demo.java
@@ -0,0 +1,24 @@
+package com.tyss;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void testA()
+	{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://demo.actitime.com/login.do");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.xpath("//div[.='Login ']")).click();
+	}
+}
